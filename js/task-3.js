79159d097f30c3e81d6e96d4f9a7b554ea286e68
Neo131088 <neo131088@gmail.com>
2025-07-13 13:46:38 +0200

    addjs

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,11 @@
 function getElementWidth(content, padding, border) {
-    return Number.parseFloat(content) + 2 * Number.parseFloat(padding) + 2 * Number.parseFloat(border);
+    const contentNum = Number.parseFloat(content);
+    const paddingNum = Number.parseFloat(padding);
+    const borderNum = Number.parseFloat(border);
+    
+    return contentNum + paddingNum * 2 + borderNum * 2;
 }
+
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
